bebb35bbfda1e29d4eaa4fae35c3f8c1cfa85331
Andrea Boccaccio <andrea@andreaboccaccio.com>
2012-05-21 16:24:54 +0200

    fixed license notice

--- Php/AndreaBoccaccio/Autoloader.php
+++ Php/AndreaBoccaccio/Autoloader.php
@@ -17,7 +17,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with phpmywhs.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 Class Php_AndreaBoccaccio_Autoloader {
